5aff4ba8cc31b2e79e180d80cf4592df37c34664
sw1tch3roo <chi3fno1d@gmail.com>
2022-09-24 00:26:17 +0300

    pizzasSkeletons

--- src/App.js
+++ src/App.js
@@ -8,15 +8,19 @@ import PizzaSort from './components/sort/PizzaSort';
 import axios from 'axios';
 
 import './scss/app.scss';
+import Skeleton from './components/pizzaBlock/SkeletonBlock';
 
 function App() {
   const [items, setItems] = React.useState([]); // массив пицц
+  const [isLoading, setIsLoading] = React.useState(Boolean);
 
   React.useEffect(() => {
+    setIsLoading(true);
     // useEffect() позволяет отлавливать действия (служит для первого рендера приложения  )
-    axios
-      .get('https://632cad725568d3cad88ad212.mockapi.io/items')
-      .then((response) => setItems(response.data));
+    axios.get('https://632cad725568d3cad88ad212.mockapi.io/items').then((response) => {
+      setItems(response.data);
+      setIsLoading(false);
+    }); // сохраняем пиццы в массив (изменяем состояние items)
   }, []); // второй параметр - условие (в данном случае [] - didMount), то есть функция сработает только один раз
   // при изменении массива вызывается функция (если передать items - будет бесконечный вызов функции)
   // так как каждый раз массив items обновляется (срабатывает изменение состояния - setItems(items))
@@ -32,11 +36,11 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {items === [] ? (
-              <h3>Ожидаем пиццу...</h3>
-            ) : (
-              items.map((pizza) => {
-                return (
+            {isLoading
+              ? // в данном случае создаем фейковый массив
+                // для предпоказа скелетонов пицц
+                [...new Array(10)].map((_, index) => <Skeleton key={index} />)
+              : items.map((pizza) => (
                   <PizzaBlock
                     key={pizza.id}
                     {...pizza} // spread-оператор
@@ -48,9 +52,7 @@ function App() {
                     // types={pizza.types}
                     // sizes={pizza.sizes}
                   />
-                );
-              })
-            )}
+                ))}
           </div>
         </div>
       </div>
---
+++ src/components/pizzaBlock/SkeletonBlock.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ContentLoader from 'react-content-loader';
+
+const Skeleton = (props) => (
+  <ContentLoader
+    className="pizza-block" // стили как у пицц
+    speed={2}
+    width={280}
+    height={500}
+    viewBox="0 0 280 500"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb"
+    // {...props}
+  >
+    <circle cx="135" cy="125" r="125" />
+    <rect x="0" y="270" rx="10" ry="10" width="280" height="25" />
+    <rect x="0" y="317" rx="10" ry="10" width="280" height="85" />
+    <rect x="0" y="430 " rx="10" ry="10" width="95" height="30" />
+    <rect x="125" y="422" rx="24" ry="24" width="152" height="45" />
+  </ContentLoader>
+);
+
+export default Skeleton;
